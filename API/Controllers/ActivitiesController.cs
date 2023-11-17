e1049d37aecca5438f570684bc48507610ed8536
TWhela <thomas.whelan@ntlworld.com>
2023-11-17 16:51:56 +0000

    Created create and edit api activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -14,10 +14,23 @@ namespace API.Controllers
         {
             return await Mediator.Send(new List.Query());
         }
-        [HttpGet("{id}")] //route parameter - api/activities/fdffdfd
+        [HttpGet("{id}")] //route parameter - api/activities/{id}
         public async Task<ActionResult<Activity>>GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id = id});
+        }
+        [HttpPost] // Creating entry 
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command {Activity = activity});
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok();
         }
     }
 }
