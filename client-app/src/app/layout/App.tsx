9b5141d40a6931756c6719e3052dcb9db28445f7
TWhela <thomas.whelan@ntlworld.com>
2023-11-21 17:33:58 +0000

    Added styling and ablitiy to edit existing posts

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -15,18 +18,39 @@ function App() {
     })
   }, [] )
 
+  //function for displaying activity on right hand side
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  //function for hiding activity
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
 
   return (
     <>
-    <NavBar />
+    <NavBar openForm={handleFormOpen}/>
     <Container style={{marginTop: '7em'}}>
-    <List>
-      {activities.map(activity => (
-        <List.Item key={activity.id}>
-          {activity.title}
-        </List.Item>
-      ))}
-    </List>
+      <ActivityDashboard 
+        activities={activities}
+        selectedActivity={selectedActivity}
+        selectActivity={handleSelectActivity}
+        cancelSelectActivity={handleCancelSelectActivity}
+        editMode={editMode}
+        openForm={handleFormOpen}
+        closeForm={handleFormClose}
+      />
     </Container>
     </>
   )
