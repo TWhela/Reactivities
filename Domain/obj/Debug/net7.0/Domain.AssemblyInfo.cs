54f383165196a6dcf5b8dffcbbf287a3efb3b714
TWhela <thomas.whelan@ntlworld.com>
2023-11-30 10:47:49 +0000

    refactored with mobx

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab6dff6fc56393828b4402dab9ddb780ea97c53c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -12,93 +9,20 @@ import { observer } from 'mobx-react-lite';
 function App() {
 const {activityStore} = useStore();
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
-
   useEffect(() => {
     //changed request with agent.ts
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-      //console.log(response);
-      setActivities(activities)
-      setLoading(false);
-    })
-  }, [] )
+    //moved to activityStore.ts
+    activityStore.loadActivities();
+  }, [activityStore] )
 
-  //function for displaying activity on right hand side
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  //function for hiding activity
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  //I'm not sure exactly how this works! review syntax!
-function handleCreateOrEditActivity(activity: Activity) {
-  setSubmitting(true);
-  if (activity.id) {
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      setSelectedActivity(activity);
-      setEditMode(false);
-      setSubmitting(false);
-    })
-  } else {
-    activity.id = uuid();
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-      setSubmitting(false);
-    })
-  }
-}
-
-function handleDeleteActivity(id: string) {
-  setSubmitting(true);
-  agent.Activities.delete(id).then(() => {
-    setActivities([...activities.filter(x => x.id !== id)]);
-    setSubmitting(false);
-  })
-}
-
-if (loading) return <LoadingComponent content='Loading app'/>
+if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
-    <NavBar openForm={handleFormOpen}/>
+    <NavBar/>
     <Container style={{marginTop: '7em'}}>
-      <h2>{activityStore.title}</h2>
-      <ActivityDashboard 
-        activities={activities}
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectActivity}
-        editMode={editMode}
-        openForm={handleFormOpen}
-        closeForm={handleFormClose}
-        createOrEdit={handleCreateOrEditActivity}
-        deleteActivity={handleDeleteActivity}
-        submitting={submitting}
-      />
+      <h2>Welcome to Reactivitie</h2>
+      <ActivityDashboard />
     </Container>
     </>
   )
