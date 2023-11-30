4888cdc850c2f74d78e3f0a3cecfe33ca305462b
TWhela <thomas.whelan@ntlworld.com>
2023-11-30 17:10:53 +0000

    added react-router

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35869604265ef0c0351379a060a98ff5e586f407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1462260eb39ed1f8d7abc529d84e460828a61cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
